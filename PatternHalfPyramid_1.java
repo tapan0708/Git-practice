133fa52969885b0b95479a8929d6156b97c1460a
tapan <tapanvyas555@gmail.com>
2025-03-02 12:23:12 +0530

    changes in PatternHalfPyramid

--- PatternHalfPyramid_1.java
+++ PatternHalfPyramid_1.java
@@ -6,13 +6,6 @@
 
 public class PatternHalfPyramid_1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
-                if (j <= i) {
-                    System.out.print(j + " ");
-                }
-            }
-            System.out.print("\n");
-        }
+
     }
 }
